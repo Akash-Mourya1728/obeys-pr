e7b76fa69d265c111e1a9a703e6e6d6f0d820f50
Akash-Mourya1728 <akashmourya68t@gmail.com>
2025-11-03 19:23:04 +0530

    locomative applied

--- script.js
+++ script.js
@@ -1,4 +1,40 @@
-function loadungAnimation(){
+function locomotiveAnimation(){
+    gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+
+
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+}
+locomotiveAnimation();
+function loadingAnimation(){
     
 var t1 = gsap.timeline()
 t1.from(".line h1",{
@@ -52,8 +88,12 @@ t1.from("#hero1 h1, #hero2 h1, #hero3 h2,#hero4 h1 ",{
     stragger:0.3
 })
 
+t1.from("#hero1, #hero2",{
+  opacity:0,
+}, "-=1.2");
+
 }
-loadungAnimation();
+loadingAnimation();
 
 function cursorAnimation(){
     document.addEventListener("mousemove",function(dets){
